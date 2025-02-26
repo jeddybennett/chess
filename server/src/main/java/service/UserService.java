4f7b5019ba016d4b9131cd58c787b353c73c4f11
Jeddy Bennett <jeddybennett01@gmail.com>
2025-02-25 18:20:50 -0700

    add response exception class

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,34 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.*;
+import org.eclipse.jetty.server.Authentication;
+
 public class UserService {
+    final private UserDAO userData;
+    final private AuthDAO authData;
+
+    public RegisterResult register(RegisterRequest registerRequest){
+
+
+
+    }
+
+    public LoginResult login(LoginRequest loginRequest) {
+        String userName = loginRequest.userName();
+        String password = loginRequest.passWord();
+        try {
+            if (userData.getUser(userName) == null) {
+                throw
+            }
+        } catch (DataAccessException exception){
+            throw
+        }
+    }
+
+    public void logout(LogoutRequest logoutRequest) {
+
+    }
 }
