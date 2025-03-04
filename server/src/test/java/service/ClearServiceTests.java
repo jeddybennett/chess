5c4f8ab3327c5d7b3b62c06b0321385f49c5d1c1
Jeddy Bennett <jeddybennett01@gmail.com>
2025-03-03 19:15:01 -0700

    finished clear service tests

--- server/src/test/java/service/ClearServiceTests.java
+++ server/src/test/java/service/ClearServiceTests.java
@@ -1,8 +1,16 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
-import dataaccess.UserDAO;
+import dataaccess.*;
+import exception.ResponseException;
+import model.CreateGameRequest;
+import model.CreateGameResult;
+import model.RegisterRequest;
+import model.RegisterResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClearServiceTests {
 
@@ -13,5 +21,34 @@ public class ClearServiceTests {
     GameService gameService;
     UserService userService;
     String authToken;
+
+    @BeforeEach
+    public void initialize() throws ResponseException{
+        gameDAO = new MemoryGameDAO();
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+
+        gameService = new GameService(gameDAO, authDAO);
+        userService = new UserService(userDAO, authDAO);
+        clearService = new ClearService(gameDAO, authDAO, userDAO);
+    }
+
+    @Test
+    @DisplayName("Clear - Positive")
+    void positiveClear() throws ResponseException, DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("jeddyBennett",
+                "password123", "email@gmail.com");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        String authToken = registerResult.authToken();
+        CreateGameRequest createGameRequest = new CreateGameRequest("MyChessGame", authToken);
+        CreateGameResult createGameResult = gameService.createGame(createGameRequest);
+        clearService.clear();
+
+        assertTrue(gameDAO.listGames().isEmpty());
+        assertNull(userDAO.getUser("jeddyBennett"));
+        assertNull(authDAO.getAuth(authToken));
+
+    }
 
 }
