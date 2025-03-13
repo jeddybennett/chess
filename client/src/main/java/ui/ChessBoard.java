8f262495df46c634e737529bc1d91f28922cbd46
Jeddy Bennett <jeddybennett01@gmail.com>
2025-03-13 10:36:02 -0600

    adding more logic learned from class

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,4 +1,30 @@
 package ui;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.ERASE_SCREEN;
+
 public class ChessBoard {
+
+    //plan on deleting this after you get the chessboard working
+    public static void main(String[] args){
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(ERASE_SCREEN);
+    }
+
+    //Make an Enum for Black and White ChessColor
+    //Build White first and then figure out how to draw for black
+    public static void drawWhiteSquare(){
+        //check if you need to draw a piece in this method
+    }
+
+    public static void drawBlackSquare(){
+        //check if you need to draw a piece in this method
+    }
+
+    public static void drawChessBoard(){
+        //pass in a matrix of where the pieces are located
+        //have draw BlackSquares and drawWhiteSquares look up positions in the matrix
+    }
 }
--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -8,10 +8,12 @@ import static ui.EscapeSequences.*;
 
 public class Client {
 
-    public static void main(String[] args){
-        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        out.print(ERASE_SCREEN);
-    }
+
+    //This shouldn't draw any of the chess board here
+    //This is where you should write code to display certain menus
+    //Check flags to see what level menu you're at (use an authToken)
+    //This will help you determine where the user is at
+    //play game and observe game should draw the chessBoard
 
     public static void drawHeaders(PrintStream out){
 
@@ -34,6 +36,8 @@ public class Client {
     private static void drawSquareRows(PrintStream out){
 
     }
+
+    //look for unicode chess characters
 
 
 
