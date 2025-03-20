71ab8e582f3c01479b6d6207dde06139b66bc957
Jeddy Bennett <jeddybennett01@gmail.com>
2025-03-20 10:08:13 -0600

    I AM SO CONFUSED

--- client/src/main/java/net/ClientCommunicator.java
+++ client/src/main/java/net/ClientCommunicator.java
@@ -1,4 +1,33 @@
 package net;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ClientCommunicator {
+
+
+    //listGames
+    public void postLogin(){
+
+    }
+
+    public void postRegister(){
+
+    }
+
+    public void deleteClear(){
+
+    }
+
+    public void putJoin(){
+
+    }
+
+    public static void getlistGames(){
+
+    }
+
+
+
+
 }
--- client/src/main/java/net/ServerFacade.java
+++ client/src/main/java/net/ServerFacade.java
@@ -1,6 +1,34 @@
 package net;
 
+import model.*;
+import net.ClientCommunicator.*;
+import ui.Client;
+
 public class ServerFacade {
 
     //should have 7 methods that talk to ClientCommunicator for an HTTP call
+    public RegisterResult register(RegisterRequest registerRequest){
+
+    }
+
+    public LoginResult login(LoginRequest loginRequest){
+
+    }
+
+    public void join(JoinGameRequest joinGameRequest){
+
+    }
+
+    public void clear(ClearRequest clearRequest){
+
+    }
+
+    public ListGameResult listGames(ListGameRequest listGameRequest){
+        String authToken = listGameRequest.authToken();
+        ClientCommunicator.getlistGames();
+    }
+
+
+
+
 }
