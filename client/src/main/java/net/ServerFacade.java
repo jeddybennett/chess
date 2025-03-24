2bbccd9ad2d027b4b7b410251661e58e0544b499
Jeddy Bennett <jeddybennett01@gmail.com>
2025-03-24 15:30:46 -0600

    working on serverFacade

--- client/src/main/java/net/ServerFacade.java
+++ client/src/main/java/net/ServerFacade.java
@@ -1,36 +1,77 @@
 package net;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
 import model.*;
-import net.ClientCommunicator.*;
+import net.ClientCommunicator;
 import ui.Client;
 
 public class ServerFacade {
 
     private final String serverURL;
-    public ServerFacade(String serverURL) {
-        this.serverURL = serverURL;
+    public ServerFacade(String url) {
+        serverURL = url;
     }
 
     //should have 7 methods that talk to ClientCommunicator for an HTTP call
     public RegisterResult register(RegisterRequest registerRequest){
-        return null;
+        String email = registerRequest.email();
+        String password = registerRequest.password();
+        String username = registerRequest.username();
+        var path = "/user";
+        var method = "POST";
+        return ClientCommunicator.makeRequest(method, path, registerRequest, RegisterResult.class);
     }
 
     public LoginResult login(LoginRequest loginRequest){
-        return null;
+        String username = loginRequest.username();
+        String password = loginRequest.password();
+        var path = "/session";
+        var method = "POST";
+        return ClientCommunicator.makeRequest(method, path, loginRequest, LoginResult.class);
     }
 
-    public void join(JoinGameRequest joinGameRequest){
+    public void logout(LogoutRequest logoutRequest){
+        String authToken = logoutRequest.authToken();
+        var path = "/session";
+        var method = "DELETE";
+        ClientCommunicator.makeRequest(method, path, logoutRequest, null);
+    }
+
+    public void joinGame(JoinGameRequest joinGameRequest){
+        String authToken = joinGameRequest.authToken();
+        int gameID = joinGameRequest.gameID();
+        ChessGame.TeamColor teamColor = joinGameRequest.playerColor();
+        var path = "/game";
+        var method = "PUT";
+        ClientCommunicator.makeRequest(method, path, joinGameRequest, null);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest createGameRequest){
+        String authToken = createGameRequest.authToken();
+        String gameName = createGameRequest.gameName();
+        var path = "/game";
+        var method = "POST";
+        return ClientCommunicator.makeRequest(method, path, createGameRequest, CreateGameResult.class);
+    }
+
+    public ListGameRequest listGames(ListGameRequest listGameRequest){
+        String authToken = listGameRequest.authToken();
+        var path = "/game";
+        var method = "GET";
+        return ClientCommunicator.makeRequest(method, path, listGameRequest, ListGameResult.class);
     }
 
     public void clear(ClearRequest clearRequest){
+        String authToken = clearRequest.authToken();
+        int gameID = clearRequest.gameID();
+        var path = "/db";
+        var method = "DELETE";
+        ClientCommunicator.makeRequest(method, path, clearRequest, null);
     }
 
-    public ListGameResult listGames(ListGameRequest listGameRequest){
-        String authToken = listGameRequest.authToken();
-//      ClientCommunicator.getlistGames();
-        return null;
-    }
+
 
 
 
